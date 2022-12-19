60b8f2d2534d90266aca9e9997916f55ca3201ee
gitanuraprilia <aprineplee@gmail.com>
2022-12-19 20:15:06 +0700

    initial main

--- lib/views/book_list_page.dart
+++ lib/views/book_list_page.dart
@@ -2,7 +2,6 @@
 import 'package:book_app/controllers/book_controller.dart';
 import 'package:book_app/views/detail_book_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class BookListPage extends StatefulWidget {
@@ -28,73 +27,56 @@ class _BookListPageState extends State<BookListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pink,
-        elevation: 0,
-      leading: IconButton(
-       icon: SvgPicture.asset("assets/icons/back.svg", color: Colors.white,), onPressed: (){},
-       ),
-       actions: <Widget>[
-        IconButton(
-           icon: SvgPicture.asset("assets/icons/search.svg", color: Colors.white), onPressed: (){
-            setState(() {
-              this.isSearching = !this.isSearching;
-            });
-           },
-           ),
-          IconButton(
-           icon: SvgPicture.asset("assets/icons/cart.svg", color: Colors.white), onPressed: (){},
-           ),
-       ],
-        title: !isSearching ? Text('Book Catalogue'): TextField(decoration: InputDecoration(
-          icon: Icon(Icons.search),
-          hintText: "Search Book Here", 
-          ),
-          ),
+        title: const Text("Book Catalogue"),
       ),
-      body:  Consumer<BookController>(
+      body: Consumer<BookController>(
         child: const Center(child: CircularProgressIndicator()),
-        builder: (context, controller, child)=> Container(
-        child:  bookController!. bookList == null ?
-           child:
-          
-           ListView.builder(
-            itemCount:bookController!.bookList!.books!.length,
-            itemBuilder: (context, index) {
-              final currentBook = bookController!.bookList!.books![index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => DetailBookPage(isbn:currentBook.isbn13!
-                  ),
-                  ),
-                  );
-                },
-                child: Row(
-                  children: [
-                  Image.network(currentBook.image!,
-                  height: 100,
-                  ),
-                  Expanded(
-                    
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal:12.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(currentBook.title!),
-                          Text(currentBook.subtitle!),
-                          Align (
-                            alignment: Alignment.topRight,
-                            child: Text(currentBook.price!)),
-                          ],
+        builder: (context, controlller, child) => Container(
+          child: bookController!.bookList == null
+              ? child
+              : ListView.builder(
+                  itemCount: bookController!.bookList!.books!.length,
+                  itemBuilder: (context, index) {
+                    final currentBook = bookController!.bookList!.books![index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => DetailBookPage(
+                              isbn: currentBook.isbn13!,
+                            ),
                           ),
-                    ),
-                  ),
-                      ]
-                  ),
-              );
-            
-          })),
+                        );
+                      },
+                      child: Row(
+                        children: [
+                          Image.network(
+                            currentBook.image!,
+                            height: 100,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(currentBook.title!),
+                                  Text(currentBook.subtitle!),
+                                  Align(
+                                      alignment: Alignment.topRight,
+                                      child: Text(currentBook.price!)),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+        ),
       ),
-      );
+    );
   }
 }
